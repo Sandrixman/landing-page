b280878d89cde366074c90f6e5a0b5e97056f930
Sandrixman <75435132+Sandrixman@users.noreply.github.com>
2024-02-10 21:41:03 +0000

    Deploying to gh-pages from @ Sandrixman/landing-page@ed95dcd36af125f8f45524a1daa06587facb1af7 🚀

---
+++ 404.30bde62c.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){document.querySelector(".error-page button").addEventListener("click",function(){window.location.href="/"})});
+//# sourceMappingURL=404.30bde62c.js.map
