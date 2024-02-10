ed95dcd36af125f8f45524a1daa06587facb1af7
Oleksander Novak <sandrix28101981@gmail.com>
2024-02-10 23:40:38 +0200

    resetting styles

---
+++ src/js/404.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.querySelector('.error-page button');
+
+    btn.addEventListener('click', function () {
+
+        window.location.href = "/";
+    });
+})
